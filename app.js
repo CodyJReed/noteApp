f91b2b8584ee856b24985fabb99879ba4554fc3c
Cody Reed <cody2j2reed@gmail.com>
2018-12-25 08:35:16 -0500

    Added exports.add to "notes.js"

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const fs = require("fs");
 const os = require("os");
 const notes = require("./notes.js");
 
-let user = os.userInfo();
+// let user = os.userInfo();
+//
+// // Updates or creates a "greetings" .txt file.
+// // Second @param - Object = new note for file
+// fs.appendFile(
+//   "greetings.txt",
+//   `Hello ${user.username}! You're ${notes.age}.`,
+//   err => {
+//     if (err) {
+//       console.log("Unable to write file");
+//     }
+//   }
+// );
 
-// Updates or creates a "greetings" .txt file.
-// Second @param - Object = new note for file
-fs.appendFile("greetingss.txt", `Hello ${user.username}!`, err => {
-  if (err) {
-    console.log("Unable to write file");
-  }
-});
+let res = notes.addNote();
+console.log(res);
+
+console.log(`Result: ${notes.add(6, 10)}`);
