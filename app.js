c13b3592d33bbe64e6dc985ddc6a3bb669010f27
Cody Reed <cody2j2reed@gmail.com>
2018-12-25 08:03:31 -0500

    Update app.js
    Added comment for initail commit ...appendFile method

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ console.log("starting app.");
 
 const fs = require("fs");
 
+// Updates or creates a "greets" .txt file.
+// Second @param - String = new note for file
 fs.appendFile("greets.txt", "Hello World!", err => {
   if (err) {
     console.log("Unable to write file");
