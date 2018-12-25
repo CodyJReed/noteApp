49bc5496e2225f8542c61a914c3e5f71a6d2dac1
Cody Reed <cody2j2reed@gmail.com>
2018-12-25 08:16:12 -0500

    Updated app.js
    requires node_module "os."
    
    applied object literal to second param.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 console.log("starting app.");
 
 const fs = require("fs");
+const os = require("os");
+
+let user = os.userInfo();
 
 // Updates or creates a "greets" .txt file.
-// Second @param - String = new note for file
-fs.appendFile("greets.txt", "Hello World!", err => {
+// Second @param - Object = new note for file
+fs.appendFile("greets.txt", `Hello ${user.username}!`, err => {
   if (err) {
     console.log("Unable to write file");
   }
