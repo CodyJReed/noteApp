48d4e4fb71317b7a4074c30c52bb330f0f520643
Cody Reed <cody2j2reed@gmail.com>
2018-12-25 08:18:39 -0500

    Update

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-console.log("starting app.");
+console.log("starting app.js");
 
 const fs = require("fs");
 const os = require("os");
+const notes = require("./notes.js");
 
 let user = os.userInfo();
 
-// Updates or creates a "greets" .txt file.
+// Updates or creates a "greetings" .txt file.
 // Second @param - Object = new note for file
-fs.appendFile("greets.txt", `Hello ${user.username}!`, err => {
+fs.appendFile("greetingss.txt", `Hello ${user.username}!`, err => {
   if (err) {
     console.log("Unable to write file");
   }
