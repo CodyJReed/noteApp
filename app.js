cb70d7e0d5463931dbb23772ca95bb2baa35d7bc
Cody Reed <cody2j2reed@gmail.com>
2018-12-26 13:34:11 -0500

    Update
    installed devDependency "yargs"
    
    required yargs in "app.js"
    
    assigned yargs.argv to const argv.
    
    created switch (command)

--- app.js
+++ app.js
@@ -2,5 +2,33 @@ console.log("starting app.js");
 
 const _ = require("lodash");
 const fs = require("fs");
+const yargs = require("yargs");
 
 const notes = require("./notes.js");
+
+const argv = yargs.argv;
+let command = process.argv[2];
+console.log(`Command: ${command}`);
+console.log("Yargs", argv);
+
+switch (command) {
+  case "add":
+    notes.addNote(argv.title, argv.body);
+    break;
+
+  case "list":
+    notes.getAll();
+    break;
+
+  case "read":
+    notes.getNote(argv.title);
+    break;
+
+  case "remove":
+    notes.removeNote(argv.title);
+    break;
+
+  default:
+    null;
+    break;
+}
