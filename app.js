851c81935bece76bcc644b998303306e48b7bd25
Cody Reed <cody2j2reed@gmail.com>
2018-12-26 11:52:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,20 @@ const fs = require("fs");
 const os = require("os");
 const notes = require("./notes.js");
 
-// let user = os.userInfo();
+console.log(_.isString(true));
+console.log(_.isString("Cody"));
+// // let user = os.userInfo();
+// //
+// // // Updates or creates a "greetings" .txt file.
+// // // Second @param - Object = new note for file
+// // fs.appendFile(
+// //   "greetings.txt",
+// //   `Hello ${user.username}! You're ${notes.age}.`,
+// //   err => {
+// //     if (err) {
+// //       console.log("Unable to write file");
+// //     }
+// //   }
+// // );
 //
-// // Updates or creates a "greetings" .txt file.
-// // Second @param - Object = new note for file
-// fs.appendFile(
-//   "greetings.txt",
-//   `Hello ${user.username}! You're ${notes.age}.`,
-//   err => {
-//     if (err) {
-//       console.log("Unable to write file");
-//     }
-//   }
-// );
-
-let res = notes.addNote();
-console.log(res);
-
-console.log(`Result: ${notes.add(6, 10)}`);
+// console.log(`Result: ${notes.add(6, 10)}`);
