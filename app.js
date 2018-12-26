55138e505b9c827436969158f549672ff9302d57
Cody Reed <cody2j2reed@gmail.com>
2018-12-26 12:45:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,5 @@ console.log("starting app.js");
 
 const _ = require("lodash");
 const fs = require("fs");
-const os = require("os");
+
 const notes = require("./notes.js");
-
-// console.log(_.isString(true));
-// console.log(_.isString("Cody"));
-
-let filteredArray = _.uniq(["Frank"]);
-console.log(filteredArray);
-// // let user = os.userInfo();
-// //
-// // // Updates or creates a "greetings" .txt file.
-// // // Second @param - Object = new note for file
-// // fs.appendFile(
-// //   "greetings.txt",
-// //   `Hello ${user.username}! You're ${notes.age}.`,
-// //   err => {
-// //     if (err) {
-// //       console.log("Unable to write file");
-// //     }
-// //   }
-// // );
-//
-// console.log(`Result: ${notes.add(6, 10)}`);
